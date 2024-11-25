a107786851926c4f40bdc99208f40b82dfb04df1
ElPol4k0 <pawel.wiercioch93@gmail.com>
2024-11-25 23:08:00 +0100

    add: connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+mod routes;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(routes::index))
+            .route("/hello/{name}", web::get().to(routes::hello))
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
